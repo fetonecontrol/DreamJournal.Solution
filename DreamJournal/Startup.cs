eb9a2bf1ad02e3ba419ac431f4d4d0caea21dba3
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-21 08:29:00 -0700

    add git ignore

--- DreamJournal/Startup.cs
+++ DreamJournal/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace DreamJournal.Solution
+namespace DreamJournal
 {
     public class Startup
     {
